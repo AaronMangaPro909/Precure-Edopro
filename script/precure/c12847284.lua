e7e9143f36752916b34033725fe1f8a3a5ab040a
AaronMangaPro909 <yojgamer224@gmail.com>
2026-06-20 11:54:12 +0800

    Update c12847284.lua

--- script/precure/c12847284.lua
+++ script/precure/c12847284.lua
@@ -1,19 +1,14 @@
--- Substitute XXXX with this card's 8-digit ID
+-- Cure Wink - Dragon Master Knight
 local s, id = GetID()
 
--- Card IDs
-local CARD_BEUD                 = 23995346 -- Official Blue-Eyes Ultimate Dragon ID
-local CARD_CURE_WINK            = 21287436 -- !! CHANGE THIS !! Replace with Cure Wink's ID
-local CARD_CURE_WINK_DRAGOON    = 84970729 -- !! CHANGE THIS !! Replace with Dragoon Armor Style's ID
+local CARD_BEUD                 = 23995346 
+local CARD_CURE_WINK            = 21287436 
+local CARD_CURE_WINK_DRAGOON    = 84970729
 
 function s.initial_effect(c)
-    -- Fusion Summon Material Setup
     c:EnableReviveLimit()
-    -- Material 1: Blue-Eyes Ultimate Dragon
-    -- Material 2: Cure Wink OR Cure Wink - Dragoon Armor Style
     Fusion.AddProcMix(c, true, true, CARD_BEUD, {CARD_CURE_WINK, CARD_CURE_WINK_DRAGOON})
-    
-    -- Must first be Fusion Summoned
+
     local e0 = Effect.CreateEffect(c)
     e0:SetType(EFFECT_TYPE_SINGLE)
     e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
@@ -21,11 +16,10 @@ function s.initial_effect(c)
     e0:SetValue(aux.fuslimit)
     c:RegisterEffect(e0)
     
-    -- 1. If Fusion Summoned: Pay 900 LP to gain 500 ATK
     local e1 = Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id, 0))
     e1:SetCategory(CATEGORY_ATKCHANGE)
-    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O) -- Optional trigger ("You can...")
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
     e1:SetProperty(EFFECT_FLAG_DELAY)
     e1:SetCondition(s.atkcon)
@@ -33,17 +27,15 @@ function s.initial_effect(c)
     e1:SetOperation(s.atkop)
     c:RegisterEffect(e1)
     
-    -- 2. Second attack during each Battle Phase
     local e2 = Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_SINGLE)
     e2:SetCode(EFFECT_EXTRA_ATTACK)
     e2:SetValue(1)
     c:RegisterEffect(e2)
     
-    -- 3. Mandatory Maintenance Cost: Standby Phase pay 900 LP or destroy
     local e3 = Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id, 1))
-    e3:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_F) -- Mandatory field trigger ("You must...")
+    e3:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_F) 
     e3:SetCode(EVENT_PHASE + PHASE_STANDBY)
     e3:SetRange(LOCATION_MZONE)
     e3:SetCountLimit(1)
@@ -52,7 +44,6 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
--- 1. Summon ATK Boost Logic
 function s.atkcon(e, tp, eg, ep, ev, re, r, rp)
     return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
 end
