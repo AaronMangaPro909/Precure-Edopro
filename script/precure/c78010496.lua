82f58eae706b23b15b84b5e979a3be606f2d074c
AaronMangaPro909 <yojgamer224@gmail.com>
2026-06-28 09:23:23 +0800

    Update c78010496.lua

--- script/precure/c78010496.lua
+++ script/precure/c78010496.lua
@@ -7,74 +7,84 @@ local CARD_MERORON   = 5826302 -- !! CHANGE THIS !!
 local CARD_CURE_ZUKYOON   = 19379373 -- !! CHANGE THIS !!
 local CARD_CURE_KISS      = 60519833 -- !! CHANGE THIS !!
 
+
 function s.initial_effect(c)
-    -- 1. Ignition Effect: Tribute Purirun/Meroron to Special Summon Cure Zukyoon/Cure Kiss
+    -- Ignition Effect (Activated from your field, e.g., on a Monster, Continuous Spell, etc.)
     local e1 = Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id, 0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_MZONE + LOCATION_HAND + LOCATION_SZONE) -- Adjust location if this is a Spell/Trap or Monster
+    e1:SetRange(LOCATION_MZONE) -- Change to LOCATION_SZONE if this is a Continuous Spell/Trap
     e1:SetCost(s.spcost)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
-    
-    -- 2. Trigger Effect: If this card is sent to the GY by a card effect
-    local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id, 1))
-    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-    e2:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_DAMAGE_STEP)
-    e2:SetCode(EVENT_TO_GRAVE)
-    e2:SetCondition(s.gycon)
-    e2:SetTarget(s.gytg)
-    e2:SetOperation(s.gyop)
-    c:RegisterEffect(e2)
 end
 
 -------------------------------------------------------------------------
--- 1. TRIBUTE & SUMMON ENGINE
+-- COST FILTER & VALIDATION ENGINE
 -------------------------------------------------------------------------
 function s.costfilter(c, tp)
+    -- Can tribute Purirun or Meroron from hand or field
     return (c:IsCode(CARD_PURIRUN) or c:IsCode(CARD_MERORON)) 
         and (c:IsControler(tp) or c:IsFaceup())
 end
+
+function s.spcost(e, tp, eg, ep, ev, re, r, rp, chk)
+    -- Account for the zone being freed if the monster tributed is on the field
+    local ft = Duel.GetLocationCount(tp, LOCATION_MZONE)
+    if chk == 0 then 
+        return Duel.IsExistingMatchingCard(s.costfilter, tp, LOCATION_HAND + LOCATION_MZONE, 0, 1, nil, tp) 
+    end
+    
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_RELEASE)
+    local g = Duel.SelectMatchingCard(tp, s.costfilter, tp, LOCATION_HAND + LOCATION_MZONE, 0, 1, 1, nil, tp)
+    
+    -- Check if the tributed card was on the field to dynamically track zone availability
+    if g:GetFirst():IsLocation(LOCATION_MZONE) then 
+        e:SetLabel(1) 
+    else 
+        e:SetLabel(0) 
+    end
+    
+    Duel.Release(g, REASON_COST)
+end
+
+-------------------------------------------------------------------------
+-- TARGET & SUMMON FILTER ENGINE
+-------------------------------------------------------------------------
 function s.spfilter(c, e, tp)
+    -- Target either Cure Zukyoon or Cure Kiss
     return (c:IsCode(CARD_CURE_ZUKYOON) or c:IsCode(CARD_CURE_KISS))
         and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
 end
-function s.spcost(e, tp, eg, ep, ev, re, r, rp, chk)
-    -- Check for tribute material from Hand or Field
-    if chk == 0 then return Duel.CheckReleaseGroupCost(tp, s.costfilter, 1, false, nil, nil, tp) end
-    local g = Duel.SelectReleaseGroupCost(tp, s.costfilter, 1, 1, false, nil, nil, tp)
-    Duel.Release(g, REASON_COST)
-end
+
 function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
-        and Duel.IsExistingMatchingCard(s.spfilter, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, nil, e, tp) end
+    -- Adjust required zone count based on whether a field zone was cleared in the cost step
+    local check_field = e:GetLabel()
+    local zone_modifier = 0
+    if check_field == 1 then zone_modifier = 1 end
+    
+    if chk == 0 then 
+        -- Reset label to avoid safety leaks during nested check iterations
+        e:SetLabel(0)
+        return (Duel.GetLocationCount(tp, LOCATION_MZONE) + zone_modifier) > 0
+            and Duel.IsExistingMatchingCard(s.spfilter, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, nil, e, tp)
+    end
+    
+    e:SetLabel(0) -- Flush variable
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_HAND + LOCATION_DECK)
 end
+
+-------------------------------------------------------------------------
+-- EXECUTION OPERATION BLOCK
+-------------------------------------------------------------------------
 function s.spop(e, tp, eg, ep, ev, re, r, rp)
     if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
+    
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
     local g = Duel.SelectMatchingCard(tp, s.spfilter, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, 1, nil, e, tp)
     if #g > 0 then
         Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP)
     end
-end
-
--------------------------------------------------------------------------
--- 2. GY TRIGGERS (SENT BY CARD EFFECT)
--------------------------------------------------------------------------
-function s.gycon(e, tp, eg, ep, ev, re, r, rp)
-    -- Verifies it was sent to the GY by a card effect (either player's)
-    return e:GetHandler():IsReason(REASON_EFFECT)
-end
-function s.gytg(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return true end
-    -- Insert secondary categories here if you add a specific effect payload later
-end
-function s.gyop(e, tp, eg, ep, ev, re, r, rp)
-    -- Add the code here for what you want this card to do when sent to the GY!
-    -- Example placeholder hint:
-    Duel.Hint(HINT_CARD, 0, id)
 end
