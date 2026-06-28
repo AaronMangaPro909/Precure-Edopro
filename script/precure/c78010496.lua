039427e17b94a6aca286570494f9635e76c8568d
AaronMangaPro909 <yojgamer224@gmail.com>
2026-06-28 11:15:34 +0800

    Update c78010496.lua

--- script/precure/c78010496.lua
+++ script/precure/c78010496.lua
@@ -7,14 +7,12 @@ local CARD_MERORON   = 5826302 -- !! CHANGE THIS !!
 local CARD_CURE_ZUKYOON   = 19379373 -- !! CHANGE THIS !!
 local CARD_CURE_KISS      = 60519833 -- !! CHANGE THIS !!
 
-
 function s.initial_effect(c)
-    -- Ignition Effect (Activated from your field, e.g., on a Monster, Continuous Spell, etc.)
+    -- Activate effect
     local e1 = Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id, 0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_MZONE) -- Change to LOCATION_SZONE if this is a Continuous Spell/Trap
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetCost(s.spcost)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
@@ -22,63 +20,36 @@ function s.initial_effect(c)
 end
 
 -------------------------------------------------------------------------
--- COST FILTER & VALIDATION ENGINE
+-- TRIBUTE COST HANDLING
 -------------------------------------------------------------------------
-function s.costfilter(c, tp)
-    -- Can tribute Purirun or Meroron from hand or field
-    return (c:IsCode(CARD_PURIRUN) or c:IsCode(CARD_MERORON)) 
-        and (c:IsControler(tp) or c:IsFaceup())
+function s.costfilter(c)
+    -- Can be "Purirun" or "Meroron", and must be able to be Tributed from Hand or Field
+    return (c:IsCode(CARD_PURIRUN) or c:IsCode(CARD_MERORON)) and c:IsReleasable()
 end
-
 function s.spcost(e, tp, eg, ep, ev, re, r, rp, chk)
-    -- Account for the zone being freed if the monster tributed is on the field
-    local ft = Duel.GetLocationCount(tp, LOCATION_MZONE)
-    if chk == 0 then 
-        return Duel.IsExistingMatchingCard(s.costfilter, tp, LOCATION_HAND + LOCATION_MZONE, 0, 1, nil, tp) 
-    end
-    
+    -- We pass e:GetHandler() to ignore this Spell card itself if it is in the hand
+    if chk == 0 then return Duel.IsExistingMatchingCard(s.costfilter, tp, LOCATION_HAND + LOCATION_MZONE, 0, 1, e:GetHandler()) end
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_RELEASE)
-    local g = Duel.SelectMatchingCard(tp, s.costfilter, tp, LOCATION_HAND + LOCATION_MZONE, 0, 1, 1, nil, tp)
-    
-    -- Check if the tributed card was on the field to dynamically track zone availability
-    if g:GetFirst():IsLocation(LOCATION_MZONE) then 
-        e:SetLabel(1) 
-    else 
-        e:SetLabel(0) 
-    end
-    
+    local g = Duel.SelectMatchingCard(tp, s.costfilter, tp, LOCATION_HAND + LOCATION_MZONE, 0, 1, 1, e:GetHandler())
     Duel.Release(g, REASON_COST)
 end
 
 -------------------------------------------------------------------------
--- TARGET & SUMMON FILTER ENGINE
+-- TARGET & SPECIAL SUMMON LOGIC
 -------------------------------------------------------------------------
 function s.spfilter(c, e, tp)
-    -- Target either Cure Zukyoon or Cure Kiss
-    return (c:IsCode(CARD_CURE_ZUKYOON) or c:IsCode(CARD_CURE_KISS))
+    -- Can be "Cure Zukyoon" or "Cure Kiss", and must be able to be Special Summoned
+    return (c:IsCode(CARD_CURE_ZUKYOON) or c:IsCode(CARD_CURE_KISS)) 
         and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
 end
-
 function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
-    -- Adjust required zone count based on whether a field zone was cleared in the cost step
-    local check_field = e:GetLabel()
-    local zone_modifier = 0
-    if check_field == 1 then zone_modifier = 1 end
-    
     if chk == 0 then 
-        -- Reset label to avoid safety leaks during nested check iterations
-        e:SetLabel(0)
-        return (Duel.GetLocationCount(tp, LOCATION_MZONE) + zone_modifier) > 0
-            and Duel.IsExistingMatchingCard(s.spfilter, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, nil, e, tp)
+        -- Standard check ensuring you have a zone open and a legal summon target in Hand or Deck
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
+            and Duel.IsExistingMatchingCard(s.spfilter, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, nil, e, tp) 
     end
-    
-    e:SetLabel(0) -- Flush variable
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_HAND + LOCATION_DECK)
 end
-
--------------------------------------------------------------------------
--- EXECUTION OPERATION BLOCK
--------------------------------------------------------------------------
 function s.spop(e, tp, eg, ep, ev, re, r, rp)
     if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
     
