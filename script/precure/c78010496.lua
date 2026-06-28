c68aa0cb0c845380a543c234e49947384952ef60
AaronMangaPro909 <yojgamer224@gmail.com>
2026-06-28 11:21:08 +0800

    Update c78010496.lua

--- script/precure/c78010496.lua
+++ script/precure/c78010496.lua
@@ -8,54 +8,87 @@ local CARD_CURE_ZUKYOON   = 19379373 -- !! CHANGE THIS !!
 local CARD_CURE_KISS      = 60519833 -- !! CHANGE THIS !!
 
 function s.initial_effect(c)
-    -- Activate effect
+    -- Activate: Send Purirun and Meroron to GY -> Special Summon Zukyoon and Kiss
     local e1 = Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_TOGRAVE)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetCost(s.spcost)
-    e1:SetTarget(s.sptg)
-    e1:SetOperation(s.spop)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
-end
-
--------------------------------------------------------------------------
--- TRIBUTE COST HANDLING
--------------------------------------------------------------------------
-function s.costfilter(c)
-    -- Can be "Purirun" or "Meroron", and must be able to be Tributed from Hand or Field
-    return (c:IsCode(CARD_PURIRUN) or c:IsCode(CARD_MERORON)) and c:IsReleasable()
-end
-function s.spcost(e, tp, eg, ep, ev, re, r, rp, chk)
-    -- We pass e:GetHandler() to ignore this Spell card itself if it is in the hand
-    if chk == 0 then return Duel.IsExistingMatchingCard(s.costfilter, tp, LOCATION_HAND + LOCATION_MZONE, 0, 1, e:GetHandler()) end
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_RELEASE)
-    local g = Duel.SelectMatchingCard(tp, s.costfilter, tp, LOCATION_HAND + LOCATION_MZONE, 0, 1, 1, e:GetHandler())
-    Duel.Release(g, REASON_COST)
-end
-
--------------------------------------------------------------------------
--- TARGET & SPECIAL SUMMON LOGIC
--------------------------------------------------------------------------
-function s.spfilter(c, e, tp)
-    -- Can be "Cure Zukyoon" or "Cure Kiss", and must be able to be Special Summoned
-    return (c:IsCode(CARD_CURE_ZUKYOON) or c:IsCode(CARD_CURE_KISS)) 
-        and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
-end
-function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then 
-        -- Standard check ensuring you have a zone open and a legal summon target in Hand or Deck
-        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
-            and Duel.IsExistingMatchingCard(s.spfilter, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, nil, e, tp) 
-    end
-    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_HAND + LOCATION_DECK)
-end
-function s.spop(e, tp, eg, ep, ev, re, r, rp)
-    if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
     
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
-    local g = Duel.SelectMatchingCard(tp, s.spfilter, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, 1, nil, e, tp)
-    if #g > 0 then
-        Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP)
+    -- Continuous: If sent to GY by card effect, Set itself
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id, 1))
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCondition(s.setcon)
+    e2:SetTarget(s.settg)
+    e2:SetOperation(s.setop)
+    c:RegisterEffect(e2)
+end
+
+-- E1 Logic
+function s.tgfilter1(c, tp)
+    return c:IsCode(CARD_PURIRUN) and c:IsAbleToGrave()
+        and Duel.IsExistingMatchingCard(s.tgfilter2, tp, LOCATION_HAND +  LOCATION_MZONE, 0, 1, c)
+end
+function s.tgfilter2(c)
+    return c:IsCode(CARD_MERORON) and c:IsAbleToGrave()
+end
+function s.spfilter1(c, e, tp)
+    return c:IsCode(CARD_CURE_ZUKYOON) and c:IsCanBeSpecialSummoned(e, 0, tp, true, false)
+        and Duel.IsExistingMatchingCard(s.spfilter2, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, c, e, tp)
+end
+function s.spfilter2(c, e, tp)
+    return c:IsCode(CARD_CURE_KISS) and c:IsCanBeSpecialSummoned(e, 0, tp, true, false)
+end
+function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then 
+        -- Standard check accounting for sending 2 monsters to clear zone spaces
+        local ft = Duel.GetLocationCount(tp, LOCATION_MZONE)
+        if ft < 0 then return false end
+        return Duel.IsExistingMatchingCard(s.tgfilter1, tp, LOCATION_MZONE, 0, 1, nil, tp)
+            and Duel.IsExistingMatchingCard(s.spfilter1, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, nil, e, tp)
+    end
+    Duel.SetOperationInfo(0, CATEGORY_TOGRAVE, nil, 2, tp, LOCATION_MZONE)
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 2, tp, LOCATION_HAND + LOCATION_DECK)
+end
+function s.activate(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TOGRAVE)
+    local g1 = Duel.SelectMatchingCard(tp, s.tgfilter1, tp, LOCATION_MZONE, 0, 1, 1, nil, tp)
+    if #g1 == 0 then return end
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TOGRAVE)
+    local g2 = Duel.SelectMatchingCard(tp, s.tgfilter2, tp, LOCATION_MZONE, 0, 1, 1, g1:GetFirst())
+    g1:Merge(g2)
+    
+    if Duel.SendtoGrave(g1, REASON_EFFECT) == 2 then
+        if Duel.GetLocationCount(tp, LOCATION_MZONE) < 2 then return end
+        Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+        local sc1 = Duel.SelectMatchingCard(tp, s.spfilter1, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, 1, nil, e, tp):GetFirst()
+        Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+        local sc2 = Duel.SelectMatchingCard(tp, s.spfilter2, tp, LOCATION_HAND + LOCATION_DECK, 0, 1, 1, sc1, e, tp):GetFirst()
+        
+        if sc1 and sc2 then
+            Duel.SpecialSummonStep(sc1, 0, tp, tp, true, false, LOCATION_MZONE)
+            Duel.SpecialSummonStep(sc2, 0, tp, tp, true, false, LOCATION_MZONE)
+            Duel.SpecialSummonComplete()
+        end
+    end
+end
+
+-- E2 Logic
+function s.setcon(e, tp, eg, ep, ev, re, r, rp)
+    return e:GetHandler():IsReason(REASON_EFFECT)
+end
+function s.settg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return e:GetHandler():IsSSetable() end
+    Duel.SetOperationInfo(0, CATEGORY_LEAVE_GRAVE, e:GetHandler(), 1, 0, 0)
+end
+function s.setop(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if c:IsRelateToEffect(e) and c:IsSSetable() then
+        Duel.SSet(tp, c)
     end
 end
