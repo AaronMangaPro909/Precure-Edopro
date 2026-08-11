57326e82b73463754ab5c35a0a6d67d9cce6249f
AaronMangaPro909 <yojgamer224@gmail.com>
2026-08-11 19:11:14 +0800

    Update c10586964.lua

--- script/precure/c10586964.lua
+++ script/precure/c10586964.lua
@@ -1,63 +1,48 @@
---Wink-Eyes Blue Dragon
-
+-- Wink-Eyes Blue Dragon.
 local s, id = GetID()
-
 local CARD_CURE_WINK = 21287436
-
 function s.initial_effect(c)
     local e1 = Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id, 0))
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_SUMMON_PROC)
-    e1:SetCondition(s.ntcon)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
-
     local e2 = Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id, 1))
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-    e2:SetCode(EFFECT_SPSUMMON_PROC)
-    e2:SetRange(LOCATION_HAND)
-    e2:SetCondition(s.spcon)
-    e2:SetTarget(s.sptg)
-    e2:SetOperation(s.spop)
+    e2:SetCategory(CATEGORY_EQUIP + CATEGORY_ATKCHANGE)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_SUMMON_SUCCESS)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetTarget(s.eqtg)
+    e2:SetOperation(s.eqop)
     c:RegisterEffect(e2)
-
-    local e3 = Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id, 2))
-    e3:SetCategory(CATEGORY_EQUIP + CATEGORY_ATKCHANGE)
-    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-    e3:SetCode(EVENT_SUMMON_SUCCESS)
-    e3:SetProperty(EFFECT_FLAG_DELAY)
-    e3:SetTarget(s.eqtg)
-    e3:SetOperation(s.eqop)
+    local e3 = e2:Clone()
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
     c:RegisterEffect(e3)
-    local e4 = e3:Clone()
-    e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+    local e4 = e2:Clone()
+    e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
     c:RegisterEffect(e4)
-    local e5 = e3:Clone()
-    e5:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    local e5 = Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id, 2))
+    e5:SetCategory(CATEGORY_NEGATE + CATEGORY_DESTROY + CATEGORY_DAMAGE)
+    e5:SetType(EFFECT_TYPE_QUICK_O)
+    e5:SetCode(EVENT_CHAINING)
+    e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DAMAGE_CAL)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetCountLimit(1)
+    e5:SetCondition(s.negcon)
+    e5:SetCost(s.negcost)
+    e5:SetTarget(s.negtg)
+    e5:SetOperation(s.negop)
     c:RegisterEffect(e5)
-
-    local e6 = Effect.CreateEffect(c)
-    e6:SetDescription(aux.Stringid(id, 3))
-    e6:SetCategory(CATEGORY_NEGATE + CATEGORY_DAMAGE)
-    e6:SetType(EFFECT_TYPE_QUICK_O)
-    e6:SetCode(EVENT_CHAINING)
-    e6:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DAMAGE_CAL)
-    e6:SetRange(LOCATION_MZONE)
-    e6:SetCountLimit(1)
-    e6:SetCondition(s.negcon)
-    e6:SetCost(s.negcost)
-    e6:SetTarget(s.negtg)
-    e6:SetOperation(s.negop)
-    c:RegisterEffect(e6)
 end
 
-function s.ntcon(e, c, minc)
-    if c == nil then return true end
-    return minc == 0 and Duel.GetLocationCount(c:GetControler(), LOCATION_MZONE) > 0
-end
+s.listed_names = {CARD_CURE_WINK}
 
 function s.spcon(e, c)
     if c == nil then return true end
@@ -65,6 +50,7 @@ function s.spcon(e, c)
     return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
         and Duel.IsExistingMatchingCard(Card.IsDiscardable, tp, LOCATION_HAND, 0, 1, c)
 end
+
 function s.sptg(e, tp, eg, ep, ev, re, r, rp, c)
     local g = Duel.GetMatchingGroup(Card.IsDiscardable, tp, LOCATION_HAND, 0, c)
     local sg = aux.SelectUnselectGroup(g, e, tp, 1, 1, nil, 1, tp, HINTMSG_DISCARD, nil, nil, true)
@@ -75,6 +61,7 @@ function s.sptg(e, tp, eg, ep, ev, re, r, rp, c)
     end
     return false
 end
+
 function s.spop(e, tp, eg, ep, ev, re, r, rp, c)
     local sg = e:GetLabelObject()
     if not sg then return end
@@ -84,11 +71,13 @@ end
 function s.eqfilter(c)
     return c:IsCode(CARD_CURE_WINK) and not c:IsForbidden()
 end
+
 function s.eqtg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_SZONE) > 0
         and Duel.IsExistingMatchingCard(s.eqfilter, tp, LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE, 0, 1, nil) end
     Duel.SetOperationInfo(0, CATEGORY_EQUIP, nil, 1, tp, LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE)
 end
+
 function s.eqop(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if Duel.GetLocationCount(tp, LOCATION_SZONE) <= 0 or c:IsFacedown() or not c:IsRelateToEffect(e) then return end
@@ -112,6 +101,7 @@ function s.eqop(e, tp, eg, ep, ev, re, r, rp)
         c:RegisterEffect(e2)
     end
 end
+
 function s.eqlimit(e, c)
     return e:GetOwner() == c
 end
@@ -119,17 +109,25 @@ end
 function s.negcon(e, tp, eg, ep, ev, re, r, rp)
     return rp ~= tp and Duel.IsChainNegatable(ev)
 end
+
 function s.negcost(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable, tp, LOCATION_HAND, 0, 1, nil) end
     Duel.DiscardHand(tp, Card.IsDiscardable, 1, 1, REASON_COST + REASON_DISCARD)
 end
+
 function s.negtg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return true end
     Duel.SetOperationInfo(0, CATEGORY_NEGATE, eg, 1, 0, 0)
+    if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+        Duel.SetOperationInfo(0, CATEGORY_DESTROY, eg, 1, 0, 0)
+    end
     Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, 600)
 end
+
 function s.negop(e, tp, eg, ep, ev, re, r, rp)
-    if Duel.NegateActivation(ev) then
-        Duel.Damage(1 - tp, 600, REASON_EFFECT)
+    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+        if Duel.Destroy(eg, REASON_EFFECT) > 0 then
+            Duel.Damage(1 - tp, 600, REASON_EFFECT)
+        end
     end
 end
